e744ae3d23ee9b10ce8510b7c0fc79662d5157fb
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-08-02 12:28:29 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -4,26 +4,40 @@ import { Link } from 'react-router-dom';
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [error, setError] = useState(false);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+        if(email.trim() === '' || password.trim() === ''){
+            return;
+        }
+
+        localStorage.setItem('user', JSON.stringify({email, password}));
+        setEmail('');
+        setPassword('');
+    }
 
     return (
         <div className='register'>
             <h2>Register</h2>
-            <form className='sign-up-form'>
-                <p className='error-para'>"Email or password isn't entered!"</p>
+            <form className='sign-up-form' onSubmit={handleSubmit}>
+            {isSubmitted && (email.trim() === '' || password.trim() === '') && (
+          <p className="error-para">"Email or password isn't entered!"</p>
+        )}
                 <div className='email-div'>
                     <label htmlFor="email">Email: </label>
-                    <input type="email" id="email" />
+                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className='password-div'>
                     <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <button type="submit" className='register-btn'>Register</button>
             </form>
             <div>
                 Already have an account?
-                <Link><button className='login-link'>Login</button></Link>
+                <Link to="/login"><button className='login-link'>Login</button></Link>
             </div>
         </div>
     )
